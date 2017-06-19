9b480deeee77e9a2ba8634885a21c7c3821ec172
andersonrsoares <anderson.r.soares@gmail.com>
2017-06-19 18:33:33 -0300

    Models

---
+++ firebase/testeunityfirebase/Assets/Model/Tabuleiro.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public class Tabuleiro  {
+
+	List<Peca> pecasP1 = new List<Peca>();
+	List<Peca> pecasP2 = new List<Peca>();
+
+}
